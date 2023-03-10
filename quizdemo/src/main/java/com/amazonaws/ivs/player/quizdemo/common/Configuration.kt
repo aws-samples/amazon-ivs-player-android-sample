48d92c4f37791355fca97770a975f1f3b325e44b
wongws <wongws@hotmail.com>
2023-03-10 14:50:51 -0800

    Update quiz demo stream URL

--- quizdemo/src/main/java/com/amazonaws/ivs/player/quizdemo/common/Configuration.kt
+++ quizdemo/src/main/java/com/amazonaws/ivs/player/quizdemo/common/Configuration.kt
@@ -4,10 +4,10 @@ object Configuration {
     const val TAG = "QuizDemo"
 
     const val DEFAULT = "Pre-defined stream 1"
-    const val LINK = "https://fcc3ddae59ed.us-west-2.playback.live-video.net/api/video/v1/us-west-2.893648527354.channel.xhP3ExfcX8ON.m3u8"
+    const val LINK = "https://4c62a87c1810.us-west-2.playback.live-video.net/api/video/v1/us-west-2.049054135175.channel.GHRwjPylmdXm.m3u8"
 
     const val PORTRAIT_OPTION = "Pre-defined stream 2"
-    const val LIVE_PORTRAIT_LINK = "https://fcc3ddae59ed.us-west-2.playback.live-video.net/api/video/v1/us-west-2.893648527354.channel.xhP3ExfcX8ON.m3u8?allow_source=true"
+    const val LIVE_PORTRAIT_LINK = "https://4c62a87c1810.us-west-2.playback.live-video.net/api/video/v1/us-west-2.049054135175.channel.GHRwjPylmdXm.m3u8?allow_source=true"
 
     const val ANSWER_DELAY = 1500L
     const val ANIMATION_DURATION = 300L
